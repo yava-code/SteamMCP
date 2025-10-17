599c38cfc27b904837858de883f41af3ca1a85d2
Error Lover <soherito@proton.me>
2025-10-17 21:51:22 +0200

    add fetch_top_market function and update server to include market data

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 from fastmcp import FastMCP
 
 from fetcher import fetch_steam_profile
+from market import fetch_top_market
 
 mcp = FastMCP(
     name="steamcp"
@@ -18,6 +19,19 @@ def get_profile_info(id: str) -> dict:
         id: Steam ID
     """
     return fetch_steam_profile(id)
+
+
+@mcp.tool()
+def get_profile_info(id: str) -> dict:
+    """
+    Fetch top 10 market items
+
+    Args:
+
+
+
+    """
+    return fetch_top_market()
 
 
 
