26c975ab2f339129783967a4262de7b8b054b18d
Error Lover <soherito@proton.me>
2025-10-17 20:56:28 +0200

    add functions to fetch friend list, player achievements, user stats, owned games, and recently played games

--- fetch_profile.py
+++ fetch_profile.py
@@ -18,3 +18,52 @@ def fetch_steam_profile(steam_id: str) -> dict:
 
 
 
+def fetch_friend_list (steam_id: str) -> dict:
+    import requests
+    #fetch friend list
+    response = requests.get(f"http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={api_key}&steamid={steam_id}&relationship=friend")
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        raise Exception(f"Failed to fetch data: {response.status_code}")
+
+
+def fetch_player_achievements(steam_id: str, appid: int) -> dict:
+    import requests
+    response = requests.get(f"http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid={appid}&key={api_key}&steamid={steam_id}")
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        raise Exception(f"Failed to fetch data: {response.status_code}")
+
+
+def fetch_user_stats_for_game(steam_id: str, appid: int) -> dict:
+    import requests
+    response = requests.get(f"http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid={appid}&key={api_key}&steamid={steam_id}")
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        raise Exception(f"Failed to fetch data: {response.status_code}")
+
+
+def fetch_owned_games(steam_id: str) -> dict:
+    import requests
+    response = requests.get(f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={steam_id}&format=json")
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        raise Exception(f"Failed to fetch data: {response.status_code}")
+
+
+def fetch_recently_played_games(steam_id: str) -> dict:
+    import requests
+    response = requests.get(f"http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key={api_key}&steamid={steam_id}&format=json")
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        raise Exception(f"Failed to fetch data: {response.status_code}")
